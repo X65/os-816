b47366e389ebe2a4bac1ccf56bf8212861ee365d
Tomasz Sterna <tomasz@sterna.link>
2025-04-11 09:28:51 +0200

    Move shell text printing to separate file

--- src/shell/main.asm
+++ src/shell/main.asm
@@ -4,10 +4,9 @@
 ; If the command is found it is executed.
 
 .import shell_commands, shell_find_command, shell_error
+.importzp shell_str_ptr
 
-.exportzp shell_str_ptr
-.export print_string, shell_print, shell_tokens
-.export print_hex, print_char
+.export shell_tokens
 .export shell_main
 
 .include "macros.inc"
@@ -15,10 +14,6 @@
 .include "kernel/api.inc"
 
 .macpack generic
-
-.zeropage
-shell_str_ptr:
-    .addr 0
 
 .segment "RODATA"
 shell_task_name:
@@ -156,71 +151,3 @@ shell_tokenize_exit:
     _ai16               ; restore regs to 16 bit before returning
     stz shell_tokens,x  ; nul-terminate the tokens
     rts
-
-; --------------------------------------------
-
-; Print a nul-terminated string
-; pointed by Accumulator
-print_string:
-    sta shell_str_ptr
-
-    ; Print a nul-terminated string
-    ; pointed by zp:shell_str_ptr
-shell_print:
-    _a8
-    ; Find the length of the string
-    ldy #0
-:   lda (shell_str_ptr),y
-    beq :+
-    iny
-    bra :-
-:   phy
-    _a16
-    lda shell_str_ptr
-    pha
-    lda #CON_WRITE
-    cop $21
-    rts
-
-; --------------------------------------------
-print_str_buf:
-    .res 4
-
-; --------------------------------------------
-; Print a number in .A in HEX
-print_hex:
-    _a8
-    pha         ; save the number
-    ; low nybble
-    and #$0F    ; extract low nybble
-    cmp #10     ; less than 10?
-    blt :+      ; use digit
-    add #('A'-10)   ; convert to letter
-    bra print_hex_next
-:   add #'0'        ; convert to digit
-print_hex_next:
-    sta print_str_buf+1
-    ; high nybble
-    pla
-    lsr         ; extract high nybble
-    lsr
-    lsr
-    lsr
-    cmp #10     ; less than 10?
-    blt :+      ; use digit
-    add #('A'-10)   ; convert to letter
-    bra print_hex_finish
-:   add #'0'        ; convert to digit
-print_hex_finish:
-    sta print_str_buf
-    stz print_str_buf+2 ; nul-terminator
-    _a16
-    lda #print_str_buf
-    jmp print_string
-
-; --------------------------------------------
-print_char:
-    and #$00ff      ; lower byte + nul-terminator
-    sta print_str_buf
-    lda #print_str_buf
-    jmp print_string
